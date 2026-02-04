15bd0db7567a0123ee13350ec0e92a1a5b422c7e
shilo0h <klaos.lleshi@fshnstudent.info>
2026-02-04 16:33:02 +0100

    First commit

---
+++ src/Users.java
@@ -0,0 +1,12 @@
+final class Users {
+
+    private final String name;
+    private final int age;
+
+
+    Users(String name1, int age1){
+
+        this.name = name1;
+        this.age = age1;
+    }
+}
