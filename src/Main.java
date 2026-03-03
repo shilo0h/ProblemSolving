a501aaefc685e24f5c79c7af70e943d463e13a97
shilo0h <klaos.lleshi@fshnstudent.info>
2026-03-03 10:27:47 +0100

    Added Stack implementation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
-class Main {
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
 
-    public static void main() {
-        System.out.println("Hello");
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] nums = {98, 2, 5, 7, 4, 2, 6, 87, 2, 3, 5, 6};
+
+        for (int num : nums) {
+            System.out.print(num + "  ");
+        }
     }
 }
---
+++ src/MyStack.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class MyStack {
+
+    private int[] arr;
+
+    // maximum size of stack
+    private int capacity;
+
+    // index of top element
+    private int top;
+
+    // constructor
+    public MyStack(int cap) {
+        capacity = cap;
+        arr = new int[capacity];
+        top = -1;
+    }
+
+    void push(int x) {
+        if (top == capacity - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        arr[++top] = x;
+    }
+
+    void print(){
+            for (int i=0;i<arr.length;i++){
+                System.out.println(arr[i]);
+            }
+        }
+}
