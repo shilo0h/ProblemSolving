be15ce3cd2366fd1a9afb0e61fd2ef32e23ebc15
shilo0h <klaos.lleshi@fshnstudent.info>
2026-03-19 10:58:09 +0100

    Added Java streams

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,6 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-            List<Integer> nums=new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,20));
-            List<Integer>biggerThenFour=nums.stream().filter(n->n<=4).map(n->n*n)
-                    .collect(Collectors.toList());
-        System.out.println(biggerThenFour);
+
     }
 }
--- src/streams/FunctionalProgramming.java
+++ src/streams/FunctionalProgramming.java
@@ -1,14 +1,41 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FunctionalProgramming {
     static void main() {
-        List<Integer> nums=new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,20));
-        List<Integer>biggerThenFour=nums.stream().filter(n->n<=4).map(n->n*n)
+        //Is the way to make use of Java stream APIs to make the
+        //so that we write less code
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+        // Create a stream
+        Stream<Integer> stream2 = nums.stream();
+
+        // Map each number to its square and collect to a list
+        List<Integer> nums3 = stream2.map(n -> n * n)
                 .collect(Collectors.toList());
-        System.out.println(biggerThenFour);
+
+
+        List<String> items = Arrays.asList("Apple", "Banana", "Cherry", "Date");
+
+            // Filter strings with length > 5
+                    List<String> longItems = items.stream()
+                            .filter(s -> s.length() > 5)
+                            .collect(Collectors.toList());
+
+                    longItems.forEach(System.out::println);
+            // Output: Banana, Cherry
+
+        // Print result
+        System.out.println(nums3); // Output: [1, 4, 9]
     }
 }
