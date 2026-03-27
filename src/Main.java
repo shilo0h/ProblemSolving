9df195e7a5f8d870473547468c17215587631845
shilo0h <klaos.lleshi@fshnstudent.info>
2026-03-27 16:03:36 +0100

    Added Calculator programm

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
+import smallProjects.calculator.Calculator;
+import streams.FunctionalProgramming;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+
 
     }
 }
---
+++ src/smallProjects/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package smallProjects.calculator;
+
+public class Calculator {
+
+    public static void add(int num1,int num2){
+        int sum=num1+num2;
+        System.out.println(sum);
+    }
+    public static void minus(int num1,int num2){
+        int sum=num1-num2;
+        System.out.println(sum);
+    }
+    public static void multiply(int num1,int num2){
+        int sum=num1*num2;
+        System.out.println(sum);
+    }
+    public static void devide(int num1,int num2){
+        int sum=num1/num2;
+        System.out.println(sum);
+    }
+}
