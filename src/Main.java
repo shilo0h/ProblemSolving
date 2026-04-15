8e6dc5217780d6a13e4671775d328bc4adcf0fab
shilo0h <klaos.lleshi@fshnstudent.info>
2026-04-15 15:53:41 +0200

    Updated some files

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner sc=new Scanner(System.in);
 
     }
 }
---
+++ src/threads/MyConcurrentHashMap.java
@@ -0,0 +1,10 @@
+package threads;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MyConcurrentHashMap {
+
+    ConcurrentHashMap<String,String>myConcurrentHashMap=new ConcurrentHashMap<>();
+
+    //TODO Learn about these
+}
