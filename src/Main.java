491d406d56c3ce8c4028703ff22140cf7addb808
shilo0h <klaos.lleshi@fshnstudent.info>
2026-04-27 17:43:01 +0200

    Updated some files

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,25 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        int []nums={1,2,3,4,5,6,7,8,9};
 
+        int numToFind=-1;
+
+        int beggining=0;
+        int end= nums.length-1;
+
+        while (beggining<=end){
+            int middle=(beggining+end)/2;
+            if(numToFind==nums[middle]){
+                System.out.println("Found at "+middle);
+                return;
+            }
+            if (numToFind<nums[middle]){
+                end=middle-1;
+            }else{
+                beggining=middle+1;
+            }
+        }
+        System.out.println("Didt find it");
     }
 }
