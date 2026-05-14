304478332d66b1113caebab4904d99d32d840d2f
shilo0h <klaos.lleshi@fshnstudent.info>
2026-05-14 17:49:51 +0200

    Added some docs files

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,35 @@ public class Main {
 
 
     private static boolean isValidSudoku(char[][] board) {
+
+        for(int i=0;i<9;i++){
+            Set<Character>set=new HashSet<>();
+            for (int j=0;j<9;j++){
+                if (board[i][j]=='.') continue;
+                if (set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') continue;
+                if (set.contains(board[j][i])) return false;
+                set.add(board[j][i]);
+            }
+        }
+        for (int square = 0; square < 9; square++) {
+            Set<Character> seen = new HashSet<>();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int row = (square / 3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    if (board[row][col] == '.') continue;
+                    if (seen.contains(board[row][col])) return false;
+                    seen.add(board[row][col]);
+                }
+            }
+        }
         return true;
     }
 }
