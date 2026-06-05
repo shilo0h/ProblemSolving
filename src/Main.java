1b4626e98910bf1e07329a32359aa733615fbb7b
shilo0h <klaos.lleshi@fshnstudent.info>
2026-06-05 11:31:38 +0200

    Added some docs files

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 import java.io.*;
 import java.time.Duration;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+            int []nums={1,2,3,4,5,6,7,8,9};
+             int window=3;
 
-        String a = "hello";
-        String b = "hello";
+             int sum=0;
 
-        System.out.println(a == b); // true
+             for (int i=0;i<window;i++){
+                 sum+=nums[i];
+             }
+
+             int maxSum=sum;
+             for(int i=window;i< nums.length;i++){
+                 maxSum=maxSum-nums[i-window]+nums[i];
+
+                 if (maxSum>sum){
+                     sum=maxSum;
+                 }
+             }
+             System.out.println(sum);
     }
 }
-
