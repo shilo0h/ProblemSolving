4554ae2974953886bbb3f76d3413f9edf89e0c4c
shilo0h <klaos.lleshi@fshnstudent.info>
2026-06-16 15:29:50 +0200

    Added some docs files

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
-import java.io.*;
-import java.time.Duration;
-import java.util.*;
-
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        int[] array = {1, 1, 2, 3, 3, 3, 4, 4, 5, 6, 6, 6, 7, 8, 9};
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-            for (int num : array) {
-                if (!map.containsKey(num)) {
-                    map.put(num, 1);
-                }else{
-                    map.put(num, map.get(num) + 1);
-                }
-            }
-        System.out.println(map);
     }
 }
